a5737c261f3daec6b138d2da6a01df1a8560bf25
Vitor Sales <vitormalisa@gmail.com>
2024-05-14 17:29:18 -0300

    Feat: Requirement 1 done

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
 const app = require('./app');
 
-app.listen(3001, () => console.log('server is running on port 3001');
+app.listen(3001, () => console.log('server is running on port 3001'));
+
+app.get('/', (_req, res) => {
+    res.send();
+});
