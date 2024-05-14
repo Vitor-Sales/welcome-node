c038b6ffae98edb2e804d15e76bd80e4a36d4c60
Vitor Sales <vitormalisa@gmail.com>
2024-05-14 17:40:14 -0300

    Feat: Requirement 2 done. Though lint error

--- src/utils/fs/readJsonData.js
+++ src/utils/fs/readJsonData.js
@@ -1,5 +1,8 @@
-const readJsonData = async (_path) => {
+const fs = require('fs/promises');
 
+const readJsonData = async (path) => {
+    const contentFile = await fs.readFile(path, 'utf-8');
+    return JSON.parse(contentFile);
 };
 
 module.exports = readJsonData;
