59c8abceefc9aa9bc9760d2bcf9a2d57cd05a555
Vitor Sales <vitormalisa@gmail.com>
2024-05-14 18:47:56 -0300

    Feat: Requirement 3 done. Though lint error

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const path = require('path');
+const { log } = require('console');
+const readJsonData = require('./utils/fs/readJsonData');
 
 const app = express();
 
+const PATH = path.resolve('src', 'movies.json');
+
 // Para poder inserir informações em JSON
 app.use(express.json());
+
+app.get('/movies/:id', async (req, res) => {
+    const { id } = req.params;
+    const moviesContent = await readJsonData(PATH);
+    const findMovie = moviesContent.find((movie) => movie.id === Number(id));
+
+    if (!findMovie) {
+        return res.status(404).json({ message: 'Filme não encontrado' });
+    }
+
+    res.status(200).json(findMovie);
+});
 
 module.exports = app;
